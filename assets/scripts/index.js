6872a2b22bdc03ac3266b702119761353ac68fb4
2rprbm <114118439+2rprbm@users.noreply.github.com>
2022-12-05 22:25:50 -0300

    Upload Arquivos Finais
    Upload de todos os arquivos usados no projeto.
    Site responsivo usando flexbox, grid, bootstrap e um pouco de JS.
    Meu primeiro #7DaysOfCode da Alura - HTML e CSS

---
+++ assets/scripts/index.js
@@ -0,0 +1,7 @@
+var botaoMenu = document.querySelector(".cabecalho__botoes--menu--icone");
+var nav = document.querySelector(".cabecalho__menu-navegacao")
+botaoMenu.addEventListener("click", function () {
+    nav.classList.toggle("cabecalho__menu-navegacao--ativo");
+  })
+ 
+  
